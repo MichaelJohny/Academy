8e6ce91208208039c643e71ad08cf23bcedf568e
Raghda Tharwat <raghdatharwat.rt@gmail.com>
2018-10-28 10:12:12 +0200

    add course screen

--- Academy.Web/Controllers/CoursesController.cs
+++ Academy.Web/Controllers/CoursesController.cs
@@ -1,17 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using Academy.Web.Models;
 
 namespace Academy.Web.Controllers
 {
     public class CoursesController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CoursesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         // GET: Courses
         public ActionResult Index()
         {
             return View();
         }
+
+
     }
 }
