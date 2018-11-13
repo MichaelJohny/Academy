d02b86ca666a435212a7d7b4e0dcfeb5a7216844
DESKTOP-RNQP152\Michael <michaeladel19@gmail.com>
2018-11-13 12:22:07 +0200

    Fix login

--- Academy.Web/Models/AccountViewModels.cs
+++ Academy.Web/Models/AccountViewModels.cs
@@ -70,12 +70,9 @@ namespace Academy.Web.Models
         [Display(Name = "Email")]
         public string Email { get; set; }
 
-        [Required]
-        [Display(Name = "UserName")]
-        public string UserName { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
